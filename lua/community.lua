3095219ec1d7487823cba8ad37f3fb419ce54e47
jakeia <azbazb011@gmail.com>
2024-04-23 18:51:23 +0800

    update

--- lua/community.lua
+++ lua/community.lua
@@ -14,12 +14,20 @@ return {
   { import = "astrocommunity.pack.markdown" },
   -- =========tool=========
   { import = "astrocommunity.fuzzy-finder.telescope-zoxide" },
+  -- { import = "astrocommunity.lsp.lsp-lens-nvim" },
+  -- { import = "astrocommunity.lsp.actions-preview-nvim" },
   { import = "astrocommunity.motion.flash-nvim" },
+  { import = "astrocommunity.motion.before-nvim" },
   { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
+  -- { import = "astrocommunity.editing-support.nvim-treesitter-context" },
   { import = "astrocommunity.file-explorer.telescope-file-browser-nvim" },
   { import = "astrocommunity.utility.noice-nvim" },
   { import = "astrocommunity.editing-support.bigfile-nvim" },
+  { import = "astrocommunity.terminal-integration.vim-tmux-yank" },
+
   -- ======colorscheme=====
-  { import = "astrocommunity.colorscheme.nordic-nvim" },
-  { import = "astrocommunity.colorscheme.rose-pine" },
+  -- { import = "astrocommunity.colorscheme.nordic-nvim" },
+  -- { import = "astrocommunity.colorscheme.rose-pine" },
+  -- { import = "astrocommunity.colorscheme.everforest" },
+  { import = "astrocommunity.colorscheme.vscode-nvim" },
 }
