d070e1b6a0697a870637931c5e8b204450cae1a2
jakeia <azbazb011@gmail.com>
2024-05-28 09:52:30 +0800

    change snippet keymaps

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,25 +1,21 @@
 return {
   "hrsh7th/nvim-cmp",
   opts = function(_, opts)
-    -- opts parameter is the default options table
-    -- the function is lazy loaded so cmp is able to be required
     local cmp = require "cmp"
     local luasnip = require "luasnip"
     local function has_words_before()
       local line, col = unpack(vim.api.nvim_win_get_cursor(0))
       return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
     end
-    -- use `extend_tbl` to easily merge into the `opts` table
     -- NOTE: this function does not merge in place and needs to be
     --       returned at the end of the function
     return require("astrocore").extend_tbl(opts, {
       mapping = {
         ["<CR>"] = cmp.config.disable,
-        -- <C-n> and <C-p> for navigating snippets
-        ["<C-n>"] = cmp.mapping(function()
+        ["<A-n>"] = cmp.mapping(function()
           if luasnip.jumpable(1) then luasnip.jump(1) end
         end, { "i", "s" }),
-        ["<C-p>"] = cmp.mapping(function()
+        ["<A-p>"] = cmp.mapping(function()
           if luasnip.jumpable(-1) then luasnip.jump(-1) end
         end, { "i", "s" }),
         ["<C-k>"] = cmp.mapping(
