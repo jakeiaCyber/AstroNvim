8180c04be92c94159f84a6d8ccdd73bdaffe3269
jakeia <azbazb011@gmail.com>
2024-06-30 10:43:42 +0800

    update cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -3,6 +3,7 @@ return {
   opts = function(_, opts)
     local cmp = require "cmp"
     local luasnip = require "luasnip"
+    local lspkind = require "lspkind"
     local function has_words_before()
       local line, col = unpack(vim.api.nvim_win_get_cursor(0))
       return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
@@ -43,6 +44,64 @@ return {
       },
       experimental = {
         ghost_text = false,
+      },
+      window = {
+        completion = cmp.config.window.bordered {
+          border = {
+            { "󱐋", "WarningMsg" },
+            { "─", "Comment" },
+            { "╮", "Comment" },
+            { "│", "Comment" },
+            { "╯", "Comment" },
+            { "─", "Comment" },
+            { "╰", "Comment" },
+            { "│", "Comment" },
+          },
+        },
+        documentation = cmp.config.window.bordered {
+          border = {
+            { "", "DiagnosticHint" },
+            { "─", "Comment" },
+            { "╮", "Comment" },
+            { "│", "Comment" },
+            { "╯", "Comment" },
+            { "─", "Comment" },
+            { "╰", "Comment" },
+            { "│", "Comment" },
+          },
+        },
+      },
+      formatting = {
+        fields = { "kind", "abbr", "menu" },
+        format = lspkind.cmp_format {
+          mode = "symbol_text",
+          preset = "codicons",
+          before = function(entry, vim_item)
+            if vim.tbl_contains({ "path" }, entry.source.name) then
+              local icon, hl_group = require("nvim-web-devicons").get_icon(entry:get_completion_item().label)
+              if icon then
+                vim_item.kind = icon
+                vim_item.kind_hl_group = hl_group
+                return vim_item
+              end
+            end
+            vim_item.menu = ({
+              nvim_lsp = "(LSP)",
+              buffer = "(Buffer)",
+              path = "(Path)",
+              luasnip = "(Snippets)",
+              calc = "(Calc)",
+            })[entry.source.name]
+
+            vim_item.dup = ({
+              buffer = 1,
+              path = 1,
+              nvim_lsp = 0,
+              snippets = 1,
+            })[entry.source.name] or 0
+            return vim_item
+          end,
+        },
       },
     })
   end,
