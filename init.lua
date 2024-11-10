f3a56a448326a4ca77afcf7a3b4e283972141310
jakeia <azbazb011@gmail.com>
2024-11-10 17:02:55 +0800

    update

--- init.lua
+++ init.lua
@@ -18,4 +18,21 @@ end
 require "lazy_setup"
 require "polish"
 
-vim.diagnostic.config { update_in_insert = false }
+vim.diagnostic.config {
+  virtual_text = {
+    prefix = "",
+    suffix = "",
+    underline = true,
+    float = { border = "single" },
+    format = function(diagnostic) return "󰍡 " .. diagnostic.message .. " " end,
+  },
+  signs = {
+    text = {
+      [vim.diagnostic.severity.HINT] = "󱐮",
+      [vim.diagnostic.severity.ERROR] = "✘",
+      [vim.diagnostic.severity.INFO] = "◉",
+      [vim.diagnostic.severity.WARN] = "",
+    },
+  },
+  update_in_insert = false,
+}
